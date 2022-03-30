25be775759f68d0b8100382794b82b83987b0b0e
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-03-30 13:22:55 -0700

    styling + form structure

--- uwu-inc/src/App.js
+++ uwu-inc/src/App.js
@@ -5,20 +5,10 @@ import Form from "./components/Form"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1>uwu inc</h1>
+      </div>
+      <Form/>
     </div>
   );
 }
