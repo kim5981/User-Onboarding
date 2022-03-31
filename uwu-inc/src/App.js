443e96f28236fc4cc8bcafb4c60abfffbad80a19
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-03-30 21:31:35 -0700

    styling, form errors displaying, set terms

--- uwu-inc/src/App.js
+++ uwu-inc/src/App.js
@@ -4,6 +4,8 @@ import User from "./components/User"
 
 import React, { useState, useEffect } from "react"
 import axios from "axios"
+import * as yup from "yup"
+import schema from "./validation/userFormSchema"
 
 function App() {
 
@@ -20,15 +22,62 @@ function App() {
   const initialUsers = [];
   const [ users, setUsers ] = useState(initialUsers)
 
+  // ...........................................          Handle Errors       ........................................................
+  const initialErrors = {
+    fname: "",
+    lname: "", 
+    email: "",
+    password: "",
+    confirm: "",
+    terms: ""
+  }
+
+  const [ formErrors, setFormErrors ] = useState(initialErrors);
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+        .then( () => setFormErrors( { ...formErrors, [name]: "" } ) )
+        .catch( err => setFormErrors( { ...formErrors, [name]: err.errors[0] } ) )
+  }
+
+  const inputChange = (name, value) => {
+    validate(name, value);
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
   const getUsers = () => {
     axios.get("https://reqres.in/api/users")
       .then( res => setUsers(res.data.data) )
       .catch( err => console.error(err) )
   }
 
+  const postNewUser = newUser => {
+    axios.post("https://reqres.in/api/users", newUser)
+      .then( res => console.log(res) )
+      .catch( err => console.error(err) )
+      .finally( () => setFormValues(intialFormValues) )
+  }
+
+  const submitForm = () => {
+    const newUser = {
+      fname: formValues.fname.trim(),
+      lname: formValues.lname.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      confirm: formValues.confirm.trim(),
+      terms: formValues.terms
+    }
+    postNewUser(newUser);
+  }
+
   useEffect( () => {
     getUsers()
   }, [])
+
+  
 
 
   return (
@@ -36,7 +85,12 @@ function App() {
       <div className="header">
         <h1>uwu inc</h1>
       </div>
-      <UserForm/>
+      <UserForm
+        values= { formValues }
+        change={ inputChange }
+        submit= { submitForm }
+        errors = { formErrors } 
+        />
       {/* {
         users.map( user => {
           return <User key={ user.id } details={ user }/>
--- uwu-inc/src/components/UserForm.js
+++ uwu-inc/src/components/UserForm.js
@@ -1,82 +1,110 @@
-import React from "react"
-
-const terms = "when you fill out this information, you swear your life and rights to privacy away to uwu inc. we pinky promise we have nothing but the best intentions for your data (●⁀‿⁀●) "
+import React, { useState } from "react"
 
 export default function UserForm (props) {
-    const { change, values, submit } = props;
+    const { change, values, submit, errors } = props;
+
 
     function onChange (evt) {
-        const { name, value } = evt.target
-        change(name, value)
+        const { name, value, checked, type } = evt.target
+        const checkValue = type === "checkbox" ? checked : value
+        change(name, checkValue)
     }
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+    }
+
+    
+    const termDetails = "when you fill out this information, you swear your life and rights to privacy away to uwu inc. we pinky promise we have nothing but the best intentions for your data (●⁀‿⁀●) ";
+    const showTerms = () => {
+        const terms = document.querySelector(".terms")
+        terms.classList.toggle("hidden")
+    }
+
 
     return (
         <>
             <div className="info-wrapper">
             <h3>♥ Welcome ♥ </h3>
+            <div className="greeting">
             <p>To create your account, please fill out all fields: </p>
-            <p className="*">Your information is precious to us <br/> (✿◠‿◠) </p>
+            <p>Your information is precious to us (✿◠‿◠) </p>
+            </div>
+            
             </div>
 
-            <form className="form-inputs" onSubmit={ submit }>
+            <form className="form-inputs" onSubmit={ onSubmit }>
                 <label> first name:
                     <input id="fname"
                         type="text"
                         name="fname"
                         placeholder="enter first name"
-                        value={""}
+                        value={values.fname}
                         onChange= { onChange }
                         />
                 </label>
+
+                <div className="error">{ errors.fname }</div>
 
                 <label> last name:
                     <input id="lname"
                         type="text"
                         name="lname"
                         placeholder="enter last name"
-                        value={""}
+                        value={values.lname}
                         onChange= { onChange }
                         />
                 </label>
+
+                <div className="error">{ errors.lname }</div>
 
                 <label>email:
                     <input id="email"
                         type="email"
                         name="email"
                         placeholder="enter email"
-                        value={""}
+                        value={values.email}
                         onChange= { onChange }
                     />
                 </label> 
+
+                <div className="error">{ errors.email }</div>
                 
                 <label>password:
                     <input id="password"
                         type="password"
                         name="password"
                         placeholder="enter password"
-                        value={""}
+                        value={values.password}
                         onChange= { onChange }
                         />
                 </label>
+
+                <div className="error">{ errors.password }</div>
 
                 <label>confirm password:
                     <input id="confirm"
                         type="password"
                         name="confirm"
                         placeholder="confirm password"
-                        value={""}
+                        value={values.confirm}
                         onChange= { onChange }
                         />
                 </label>
+
+                <div className="error">{ errors.confirm }</div>
                     
                 <label> terms of service:
                     <input
                         type="checkbox"
                         name="terms"
                         onChange={ onChange }
-                        checked= { "" }
+                        checked= { values.terms }
                     />
                 </label>
+
+                <div className="error">{ errors.terms }</div>
             
                 <div className="submission-wrap">
                     <button>
@@ -84,7 +112,13 @@ export default function UserForm (props) {
                     </button>
                 </div>
                 
+                <button onClick={ showTerms } className="termsBtn"> terms </button>
+                <p className="terms hidden">{ termDetails }</p>
+
             </form>
+
+
+               
             
         </>
     )
